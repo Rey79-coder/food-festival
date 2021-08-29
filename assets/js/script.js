9ca124fb1d942cffe2ac9dbc963bc389956e98d9
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-08-29 15:46:34 -0700

    snippet-code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,26 @@
-require("bootstrap");
-const createEl = require("./domMethods");
+require('bootstrap');
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
 
-require("bootstrap");
-
-  dateConverter = function(UNIX_timestamp) {
-    const a = new Date(UNIX_timestamp);
-    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    const year = a.getFullYear();
-    const month = months[a.getMonth()];
-    const date = a.getDate();
-    const result =  month + ' ' + date + ', ' + year 
-    return result;
-  };
-
-  if (window.location.href.indexOf("tickets") > -1) {
-
-    const purchaseBtn = document.getElementById("purchaseBtn");
-    const purchaseEmail = document.getElementById("purchaseEmail");
-    const modalEl = document.querySelector(".modal-content");
-    const modalBodyEl = document.querySelector(".modal-body");
-    const modalFooterEl = document.querySelector(".modal-footer");
-
-
-
+$(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
@@ -61,4 +41,4 @@ require("bootstrap");
     </div>
   </div>`)
   })
-};
+});
